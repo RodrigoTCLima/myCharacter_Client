f3f95f04debfabe89a44188a1d882c99d94d3c2c
Rodrigo Lima <rodrigotiagocl@gmail.com>
2025-08-17 15:34:13 -0300

    feat(auth): Implement authentication route guards
    Created and configured AuthGuard and PublicGuard to manage route access based on authentication state.
    
    - AuthGuard protects private routes, redirecting unauthenticated users to the login page.
    - PublicGuard protects public-only routes (like /login and /register), redirecting authenticated users to the home page.
    - Applied PublicGuard to the login and register routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { publicGuard } from './guards/public.guard';
 
 export const routes: Routes = [
     {
@@ -10,10 +11,12 @@ export const routes: Routes = [
     },
     {
         path: 'login',
-        component:LoginComponent
+        component: LoginComponent,
+        canActivate: [publicGuard]
     },
     {
         path: 'register',
-        component:RegisterComponent
+        component: RegisterComponent,
+        canActivate: [publicGuard]
     }
 ];
