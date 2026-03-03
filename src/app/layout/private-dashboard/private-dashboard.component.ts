7fce504d4b8c33c91f2c10bdb1b1be4063583942
RodrigoTCLima <rodrigotiagocl@gmail.com>
2026-03-03 15:28:04 -0300

    feat(private-dashboard): add role-based admin card and username display
    - Integrated isAdmin check to show/hide admin panel.
    - Displayed dynamic username in header.
    - Minor UI refinements for button contrast and layout.

--- src/app/layout/private-dashboard/private-dashboard.component.ts
+++ src/app/layout/private-dashboard/private-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CampaignListComponent } from "../../features/campaigns/campaign-list/campaign-list.component";
 import { RouterLink } from "@angular/router";
+import { AuthService } from '../../core/services/auth.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-private-dashboard',
@@ -9,5 +11,11 @@ import { RouterLink } from "@angular/router";
   styleUrl: './private-dashboard.component.scss'
 })
 export class PrivateDashboardComponent {
+  user: User | null = null;
+  isAdmin: boolean = false;
 
+  constructor(private authService: AuthService) {
+    this.user = this.authService.currentUser();
+    this.isAdmin = this.authService.isAdmin();
+  }
 }
